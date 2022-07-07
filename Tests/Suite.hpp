f12dea8d8cc3de6bda4222873f2d12c2ad5f7a40
Maciej <sppmacd@pm.me>
2022-07-07 22:45:43 +0200

    Tests: Add EXPECT_EQ

--- Tests/Suite.hpp
+++ Tests/Suite.hpp
@@ -1,8 +1,10 @@
 // No #pragma once, this is expected to be included only once per test file
 
 #include <Util/Error.hpp>
+#include <fmt/format.h>
 #include <iostream>
 #include <map>
+#include <sstream>
 #include <string_view>
 
 using namespace Util;
@@ -10,7 +12,7 @@ using namespace Util;
 namespace __TestSuite {
 
 struct TestError {
-    std::string_view expression;
+    std::string expression;
     std::string_view file;
     int line;
 };
@@ -18,7 +20,17 @@ struct TestError {
 ErrorOr<void, TestError> expect(bool condition, std::string_view expression, std::string_view file, int line)
 {
     if (!condition)
-        return TestError { expression, file, line };
+        return TestError { std::string { expression }, file, line };
+    return {};
+}
+
+ErrorOr<void, TestError> expect_equal(auto v1, auto v2, std::string_view expr1, std::string_view expr2, std::string_view file, int line)
+{
+    if (v1 != v2) {
+        std::ostringstream oss;
+        oss << expr1 << " == " << expr2 << " (lhs = " << v1 << ", rhs = " << v2 << ")";
+        return TestError { oss.str(), file, line };
+    }
     return {};
 }
 
@@ -31,6 +43,8 @@ std::map<std::string_view, Test*> tests;
 #define EXPECT(...) TRY(__TestSuite::expect(__VA_ARGS__, #__VA_ARGS__, __FILE__, __LINE__))
 constexpr bool Fail = false;
 #define FAIL() EXPECT(Fail)
+
+#define EXPECT_EQ(e1, e2) TRY(__TestSuite::expect_equal((e1), (e2), #e1, #e2, __FILE__, __LINE__))
 
 #define TEST_CASE(name)                                                                 \
     ErrorOr<void, __TestSuite::TestError> __test_##name##_func();                       \
@@ -47,7 +61,7 @@ int main()
     for (auto const& test : __TestSuite::tests) {
         auto result = test.second();
         if (result.is_error()) {
-            std::cout << "\e[31m ✗ \e[m" << test.first << ": \e[1m" << result.error().expression << "\e[m at \e[36m" << result.error().file << ":" << result.error().line << "\e[m\n";
+            std::cout << "\e[31m ✗ \e[m" << test.first << ": expected \e[1m" << result.error().expression << "\e[m at \e[36m" << result.error().file << ":" << result.error().line << "\e[m\n";
             num_failed++;
         } else {
             std::cout << "\e[32m ✓ \e[m" << test.first << "\n";
