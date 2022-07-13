04da2a0fe5f7343bb5bc462a0b3716a5eda5f67a
Maciej <sppmacd@pm.me>
2022-07-13 15:58:28 +0200

    Build: Fix include

--- cmake/EssaUtilConfig.cmake
+++ cmake/EssaUtilConfig.cmake
@@ -1,5 +1,5 @@
 if (ESSAUTIL_ENABLE_SFML)
     find_package(SFML 2.5.1 COMPONENTS system REQUIRED)
 endif()
-include(${CMAKE_CURRENT_LIST_DIR}/EssaGUI.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/EssaUtil.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/utils.cmake)
