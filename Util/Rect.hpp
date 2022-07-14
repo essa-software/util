1125c54b7b3a660b5ac9151f3339136842a9250c
Maciej <sppmacd@pm.me>
2022-07-14 13:49:56 +0200

    Util/Rect: Properly set template types

--- Util/Rect.hpp
+++ Util/Rect.hpp
@@ -32,6 +32,15 @@ public:
     {
     }
 
+    template<class U>
+    explicit Rect(Rect<U> const& other)
+        : left(other.left)
+        , top(other.top)
+        , width(other.width)
+        , height(other.height)
+    {
+    }
+
     Vector2<T> position() const { return { left, top }; }
     Vector2<T> size() const { return { width, height }; }
 
@@ -39,6 +48,18 @@ public:
     bool contains(Vector2<U> pos) const
     {
         return pos.x() >= left && pos.x() <= left + width && pos.y() >= top && pos.y() <= top + height;
+    }
+
+    template<class U = T>
+    Rect<U> intersection(Rect<U> const& other) const
+    {
+        T max_x = std::max<U>(left, other.left);
+        T max_y = std::max<U>(top, other.top);
+
+        T min_x = std::min<U>(left + width, other.left + other.width);
+        T min_y = std::min<U>(top + height, other.top + other.height);
+
+        return max_x < min_x && max_y < min_y ? Rect<U>({ max_x, max_y }, { min_x - max_x, min_y - max_y }) : Rect<U>();
     }
 };
 
