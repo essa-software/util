f78c976ad22fde01560e4dd24cc22f60034bbf35
Maciej <sppmacd@pm.me>
2022-08-21 15:05:36 +0200

    Tests: Add tests for Color::to_html_string()

---
+++ Tests/Color.cpp
@@ -0,0 +1,10 @@
+#include "Suite.hpp"
+
+#include <Util/Color.hpp>
+
+TEST_CASE(to_html_string) {
+    EXPECT_EQ(Colors::White.to_html_string().encode(), "#ffffff");
+    EXPECT_EQ(Colors::AliceBlue.to_html_string().encode(), "#f0f8ff");
+    EXPECT_EQ(Color { 0x030303ff }.to_html_string().encode(), "#030303");
+    return {};
+}
