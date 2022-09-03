ec05ec57a05aa06c4fc675c44225bd3330da0654
Maciej <sppmacd@pm.me>
2022-09-03 21:20:06 +0200

    Util: Add ScopeGuard class
    This class will run a given function on destructing.

---
+++ Tests/ScopeGuard.cpp
@@ -0,0 +1,12 @@
+#include "Suite.hpp"
+
+#include <Util/ScopeGuard.hpp>
+
+TEST_CASE(scope_guard) {
+    int a = 5;
+    {
+        ScopeGuard guard { [&a] { a = 10; } };
+    }
+    EXPECT_EQ(a, 10);
+    return {};
+}
