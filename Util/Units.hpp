66cab6e8ff4229c84b8c0a1318cde178d0b55188
Maciej <sppmacd@pm.me>
2022-09-25 21:14:04 +0200

    Util: Move Length to Unit namespace and separate header

--- Util/Units.hpp
+++ Util/Units.hpp
@@ -6,58 +6,6 @@
 #include <ostream>
 #include <string>
 
-class Length
-{
-public:
-    enum Unit
-    {
-        Initial,
-        Auto,
-        Px,
-        PxOtherSide,
-        Percent
-    };
-
-    constexpr Length() = default;
-
-    constexpr Length(Unit unit)
-        : m_unit(unit)
-    {
-    }
-
-    constexpr Length(float v, Unit unit)
-        : m_value(v)
-        , m_unit(unit)
-    {
-    }
-
-    constexpr Unit unit() const { return m_unit; }
-    constexpr float value() const { return m_value; }
-
-    constexpr bool operator==(Length const& other) const { return m_unit == other.m_unit && m_value == other.m_value; }
-
-private:
-    friend std::ostream& operator<<(std::ostream& out, Length const& l) { return out << l.m_value << "U" << (int)l.unit(); }
-
-    float m_value = 0;
-    Unit m_unit = Auto;
-};
-
-constexpr Length operator""_px(long double v)
-{
-    return Length(v, Length::Px);
-}
-
-constexpr Length operator""_px_o(long double v)
-{
-    return Length(v, Length::PxOtherSide);
-}
-
-constexpr Length operator""_perc(long double v)
-{
-    return Length(v, Length::Percent);
-}
-
 class Distance
 {
 public:
