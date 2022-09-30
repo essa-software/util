bcd8654d8bf47a3c6312772514ddf156a8117ee8
Maciej <sppmacd@pm.me>
2022-09-30 19:41:09 +0200

    Util: Add some functions for randomization

---
+++ Util/Random.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <EssaUtil/Vector.hpp>
+#include <cmath>
+#include <random>
+
+namespace Util::Random {
+
+inline auto& default_engine() {
+    static std::default_random_engine rng;
+    return rng;
+}
+
+template<std::floating_point T>
+T floating(float min, float max) {
+    return std::uniform_real_distribution<T>(min, max)(default_engine());
+}
+
+template<class T>
+Util::Vector2<T> vector2(float length) {
+    double angle = floating<T>(0, 2 * M_PI);
+    double x;
+    double y;
+    sincos(angle, &x, &y);
+    return Util::Vector2<T> { x, y } * length;
+}
+
+}
