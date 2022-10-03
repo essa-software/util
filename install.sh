c125eb159ee6b81c63dfa8d6a3669f4378eb3ac0
Maciej <sppmacd@pm.me>
2022-10-03 09:06:31 +0200

    CI: Build fmt instead of installing it from package manager
    Ubuntu's apt has outdated FMT (as always), so let's not bother with it;
    just build libraries yourself.

--- install.sh
+++ install.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
+
 if [ ! -e build ]; then
     mkdir build
 fi
 cd build
+
+# FIXME: Don't hardcode this path, instead find a better CMake-supported
+# way to check if FMT is installed.
+test -r /usr/lib/cmake/fmt/fmt-config.cmake
+FMT_FOUND=$?
+
+if [ $FMT_FOUND -eq 1 ]; then
+    git clone https://github.com/fmtlib/fmt fmt --depth 1 --branch 9.1.0
+    mkdir fmt/build
+    cd fmt/build
+    cmake .. -GNinja
+    ninja
+    sudo ninja install
+    cd ../..
+fi
+
 cmake .. -GNinja $CMAKE_OPTIONS
 ninja
 sudo ninja install
