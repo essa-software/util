1d7e64df90ba90db3c928a466312de47420f6dc3
Maciej <sppmacd@pm.me>
2022-10-06 22:42:53 +0200

    Util: Add Overloaded and DependentFalse
    Usage of these started to increase in EssaLang, so I moved these helpers
    to Util.

--- Util/Config.hpp
+++ Util/Config.hpp
@@ -2,3 +2,14 @@
 
 #define ESSA_ALWAYS_INLINE [[gnu::always_inline]]
 #define ESSA_UNREACHABLE __builtin_unreachable()
+
+namespace Util {
+
+// https://en.cppreference.com/w/cpp/utility/variant/visit
+template<class... Ts>
+struct Overloaded : Ts... { using Ts::operator()...; };
+
+template<typename... Deps>
+inline constexpr bool DependentFalse = false;
+
+}
