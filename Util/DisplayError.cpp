5d9fab76774f78207b6e2cc0bbc0b31716733285
Maciej <sppmacd@pm.me>
2022-11-25 12:04:47 +0100

    Util: Make display_error resistant for various edge cases
    end_offset < start_offset or end_offset == start_offset begin common
    when token is near EOF.

--- Util/DisplayError.cpp
+++ Util/DisplayError.cpp
@@ -19,8 +19,19 @@ void display_error(ReadableStream& input, DisplayedError error) {
         if (last_index <= error.start_offset && last_index + line.size() >= static_cast<size_t>(error.start_offset)) {
             auto line_number = reader.is_eof() ? reader.location().line : reader.location().line - 1;
             fmt::print("{:4} | ", line_number + 1);
+
+            size_t range_size = [&]() -> size_t {
+                if (error.end_offset < error.start_offset) {
+                    return line.size() - error.start_offset;
+                }
+                if (error.end_offset - error.start_offset < 1) {
+                    return 1;
+                }
+                return error.end_offset - error.start_offset;
+            }();
+
             for (ssize_t s = 0; static_cast<size_t>(s) < line.size(); s++) {
-                if (s + last_index >= error.start_offset && s + last_index < error.end_offset)
+                if (s + last_index >= error.start_offset && s + last_index < error.start_offset + range_size)
                     fmt::print("\e[31m{}\e[m", Util::UString { line.at(s) }.encode());
                 else
                     fmt::print("{}", Util::UString { line.at(s) }.encode());
@@ -29,7 +40,7 @@ void display_error(ReadableStream& input, DisplayedError error) {
             fmt::print("       ");
             for (size_t s = 0; s < error.start_offset - last_index; s++)
                 fmt::print(" ");
-            for (size_t s = 0; s < error.end_offset - error.start_offset; s++) {
+            for (size_t s = 0; s < range_size; s++) {
                 fmt::print("^");
             }
             fmt::print(" \e[31m{}\e[m\n", error.message.encode());
