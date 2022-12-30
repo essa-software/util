f8c24337e344070837b0addf5267ff2db92ade62
Maciej <sppmacd@pm.me>
2022-12-30 18:01:46 +0100

    Util/Random: Add some Engine object for "seeded random"

--- Util/Random.hpp
+++ Util/Random.hpp
@@ -6,31 +6,68 @@
 
 namespace Util::Random {
 
-inline auto& default_engine() {
-    static std::default_random_engine rng;
+template<class Random>
+class Engine {
+public:
+    template<class... Args>
+    requires requires(Args... args) { Random(args...); }
+    Engine(Args&&... args)
+        : m_std_random(std::forward<Args>(args)...) { }
+
+    // Generate random bool, where 1/`chance` of bools will be true.
+    bool next_bool(int chance) {
+        return next_int(0, chance - 1) == 0;
+    }
+
+    // Generate random T ∈ ℤ ∩ <min; max>
+    template<std::integral T>
+    T next_int(T min, T max) {
+        return std::uniform_int_distribution<T>(min, max)(m_std_random);
+    }
+
+    // Generate random T ∈ ℝ ∩ <min; max)
+    template<std::floating_point T>
+    T next_float(T min, T max) {
+        return std::uniform_real_distribution<T>(min, max)(m_std_random);
+    }
+
+    // Generate random vector [ℝ×ℝ] of length [length]
+    template<class T>
+    Util::Vector2<T> next_vec2(float length) {
+        double angle = next_float<T>(0, 2 * M_PI);
+        double x;
+        double y;
+        sincos(angle, &x, &y);
+        return Util::Vector2<T> { x, y } * length;
+    }
+
+private:
+    Random m_std_random;
+};
+
+using DefaultEngine = Engine<std::default_random_engine>;
+
+inline DefaultEngine& default_engine() {
+    static DefaultEngine rng;
     return rng;
 }
 
 // Generate random T ∈ ℤ ∩ <min; max>
 template<std::integral T>
 T integer(T min, T max) {
-    return std::uniform_int_distribution<T>(min, max)(default_engine());
+    return default_engine().next_int<T>(min, max);
 }
 
 // Generate random T ∈ ℝ ∩ <min; max)
 template<std::floating_point T>
 T floating(T min, T max) {
-    return std::uniform_real_distribution<T>(min, max)(default_engine());
+    return default_engine().next_float<T>(min, max);
 }
 
 // Generate random vector [ℝ×ℝ] of length [length]
 template<class T>
 Util::Vector2<T> vector2(float length) {
-    double angle = floating<T>(0, 2 * M_PI);
-    double x;
-    double y;
-    sincos(angle, &x, &y);
-    return Util::Vector2<T> { x, y } * length;
+    return default_engine().next_vec2<T>(length);
 }
 
 }
